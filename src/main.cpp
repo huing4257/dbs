91cf57674230f83d0a049a39e5f22b8b0b54729b
huying <huing4257@163.com>
2023-12-27 21:31:02 +0800

    feat: test

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 #include <cstdio>
 #include "antlr4-runtime.h"
+using namespace std;
 
-int main() {
+int main()
+{
     printf("Hello, world!\n");
+
+    for (std::string s; s != "exit"; ){
+        std::cin >> s;
+        cout << s << endl;
+    }
+    printf("exit");
     return 0;
 }
